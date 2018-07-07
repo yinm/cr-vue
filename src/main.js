323203d2a01369b21c3a75b9d0ccd25ac8359cff
yinm <yinm.cminor9@gmail.com>
2018-07-07 23:21:47 +0900

    Check string with v-for

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
 new Vue({
   el: '#app',
   data: {
-    list: [
-      { id: 1, name: 'スライム', hp: 100 },
-      { id: 2, name: 'ゴブリン', hp: 200 },
-      { id: 3, name: 'ドラゴン', hp: 500 },
-    ],
-  },
-  methods: {
-    attack(index) {
-      this.list[index].hp -= 10
-    }
+    chars: 'Vue',
   }
 })
