08fc354621291469bbc39dba81e4dd65c2de879d
yinm <yinm.cminor9@gmail.com>
2018-07-07 22:18:06 +0900

    Check object

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 new Vue({
   el: '#app',
   data: {
-    isChild: true,
-    isActive: true,
-    textColor: 'red',
-    bgColor: 'lightgray',
+    classObject: {
+      child: true,
+      'is-active': false,
+    },
+    styleObject: {
+      color: 'red',
+      backgroundColor: 'lightgray',
+    }
   }
 })
