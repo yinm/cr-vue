72225030cd6a2b601fb91c2cd9f7e24c4a8ee6e1
yinm <yinm.cminor9@gmail.com>
2018-07-07 23:48:05 +0900

    Check v-text

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 const app = new Vue({
   el: '#app',
   data: {
-    url: 'https://cr-vue.mio3io.com/guide/chapter2.html',
-    message: 'Hello Vue.js',
-  }
+    message: 'Hello Vue.js!',
+  },
 })
