e8e40567d2f76ebcaf0861ed9e05a92aa1404943
yinm <yinm.cminor9@gmail.com>
2018-07-07 23:42:38 +0900

    Check v-pre

--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
 new Vue({
   el: '#app',
   data: {
-    show: true,
-  },
-  methods: {
-    handleClick() {
-      let count = this.$refs.count
-      if (count) {
-        count.innerText = parseInt(count.innerText, 10) + 1
-      }
-    }
+    message: 'aaaaa',
   }
 })
