87c609eff0d8b2fccebabdba201b0f9552def9c5
yinm <yinm.cminor9@gmail.com>
2018-07-07 22:51:56 +0900

    Check updating list

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 new Vue({
   el: '#app',
   data: {
+    name: 'キマイラ',
     monsters: [
       { id: 1, name: 'スライム', hp: 100 },
       { id: 2, name: 'ゴブリン', hp: 200 },
       { id: 3, name: 'ドラゴン', hp: 500 },
     ],
   },
+  methods: {
+    add() {
+      let max = this.monsters.reduce((a, b) => {
+        return a > b.id ? a : b.id
+      }, 0)
+
+      this.monsters.push({
+        id: max + 1,
+        name: this.name,
+        hp: 500,
+      })
+    }
+  }
 })
