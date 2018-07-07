c19f5ff2df3b8478197dbe3092a94ef33f3049a3
yinm <yinm.cminor9@gmail.com>
2018-07-07 23:00:43 +0900

    Check splice

--- src/main.js
+++ src/main.js
@@ -1,25 +1,15 @@
 new Vue({
   el: '#app',
   data: {
-    name: 'キマイラ',
-    hp: 100,
-    monsters: [
+    list: [
       { id: 1, name: 'スライム', hp: 100 },
       { id: 2, name: 'ゴブリン', hp: 200 },
       { id: 3, name: 'ドラゴン', hp: 500 },
     ],
   },
   methods: {
-    add() {
-      let max = this.monsters.reduce((a, b) => {
-        return a > b.id ? a : b.id
-      }, 0)
-
-      this.monsters.push({
-        id: max + 1,
-        name: this.name,
-        hp: this.hp,
-      })
+    remove(index) {
+      this.list.splice(index, 1)
     }
   }
 })
