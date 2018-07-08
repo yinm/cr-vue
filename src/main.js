d2e5cce3b0aaa0f48528c313679ff697a6e3e250
yinm <yinm.cminor9@gmail.com>
2018-07-08 19:14:48 +0900

    Check $emit

--- src/main.js
+++ src/main.js
@@ -1,17 +1,34 @@
 Vue.component('comp-child', {
-  template: '<button @click="handleClick">イベント発火</button>',
-  methods: {
-    handleClick() {
-      this.$emit('childs-event')
-    },
+  template: '<li>{{ name }} HP.{{ hp }}\
+  <button @click="doAttack">攻撃する</button></li>',
+  props: {
+    id: Number,
+    name: String,
+    hp: Number,
   },
+  methods: {
+    doAttack() {
+      this.$emit('attack', this.id)
+    }
+  }
 })
 
 new Vue({
   el: '#app',
-  methods: {
-    parentsMethod() {
-      alert('イベントをキャッチ')
-    }
+  data: {
+    list: [
+      { id: 1, name: 'スライム', hp: 100 },
+      { id: 2, name: 'ゴブリン', hp: 200 },
+      { id: 3, name: 'ドラゴン', hp: 500 },
+    ],
   },
+  methods: {
+    handleAttack(id) {
+      let item = this.list.find((el) => {
+        return el.id === id
+      })
+
+      if (item !== undefined && item.hp > 0) item.hp -= 10
+    }
+  }
 })
