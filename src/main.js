311cb56f795d6acecf1f06d99e5bf52414e9643e
yinm <yinm.cminor9@gmail.com>
2018-07-08 13:00:43 +0900

    Check cache with extract process

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 new Vue({
   el: '#app',
+  data: {
+    budget: 300,
+    limit: 2,
+    list: [
+      { id: 1, name: 'apple', price: 100 },
+      { id: 2, name: 'banana', price: 200 },
+      { id: 3, name: 'strawberry', price: 400 },
+      { id: 4, name: 'orange', price: 300 },
+      { id: 5, name: 'melon', price: 500 },
+    ],
+  },
   computed: {
-    computedData() { return Math.random() },
-  },
-  methods: {
-    methodData() { return Math.random() },
-  },
+    matched() {
+      return this.list.filter(el => {
+        return el.price <= this.budget
+      })
+    },
+    limited() {
+      return this.matched.slice(0, this.limit)
+    },
+  }
 })
