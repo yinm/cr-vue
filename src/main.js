fc2d7e276df97ad238fdba7fc0cea84d876f4073
yinm <yinm.cminor9@gmail.com>
2018-07-08 13:42:25 +0900

    Check filter

--- src/main.js
+++ src/main.js
@@ -1,20 +1,11 @@
 new Vue({
   el: '#app',
   data: {
-    list: [],
-    current: '',
-    topics: [
-      { value: 'vue', name: 'Vue.js' },
-      { value: 'jQuery', name: 'jQuery' },
-    ],
+    price: 19800,
   },
-  watch: {
-    current(val) {
-      axios.get('https://api.github.com/search/repositories', {
-        params: { q: `topic:${val}` }
-      }).then(response => {
-        this.list = response.data.items
-      })
+  filters: {
+    localeNum(val) {
+      return val.toLocaleString()
     }
-  },
+  }
 })
