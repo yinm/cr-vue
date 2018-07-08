833a50f1e52d8d903be333bbaed0e482dd37168e
yinm <yinm.cminor9@gmail.com>
2018-07-08 19:08:43 +0900

    Check $emit and on (custom events)

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 Vue.component('comp-child', {
-  template: '<li>{{ name }} HP.{{ hp }}</li>',
-  props: ['name', 'hp'],
+  template: '<button @click="handleClick">イベント発火</button>',
+  methods: {
+    handleClick() {
+      this.$emit('childs-event')
+    },
+  },
 })
 
 new Vue({
   el: '#app',
-  data: {
-    list: [
-      { id: 1, name: 'スライム', hp: 100 },
-      { id: 2, name: 'ゴブリン', hp: 200 },
-      { id: 3, name: 'ドラゴン', hp: 500 },
-    ]
-  }
+  methods: {
+    parentsMethod() {
+      alert('イベントをキャッチ')
+    }
+  },
 })
