48c8e12663f053bbb26b657359aef737aa7ff931
yinm <yinm.cminor9@gmail.com>
2018-07-08 13:58:20 +0900

    Check hooks

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
-new Vue({
+Vue.directive('example', {
+  bind(el, binding) {
+    console.log('v-example bind')
+  },
+
+  inserted(el, binding) {
+    console.log('v-example inserted')
+  },
+
+  update(el, binding) {
+    console.log('v-example update')
+  },
+
+  componentUpdated(el, binding) {
+    console.log('v-example componentedUpdated')
+  },
+
+  unbind(el, binding) {
+    console.log('v-example unbind')
+  },
+})
+
+const app = new Vue({
   el: '#app',
-  directives: {
-    focus: {
-      inserted(el) {
-        el.focus()
-      }
-    }
-  }
 })
