f9874fa8d55343c3367e47bbef92ac535a7518a3
yinm <yinm.cminor9@gmail.com>
2018-07-08 12:50:59 +0900

    Check getter and setter

--- src/main.js
+++ src/main.js
@@ -2,20 +2,11 @@ const app = new Vue({
   el: '#app',
   data: {
     width: 800,
-    height: 600,
   },
   computed: {
-    halfWidth() {
-      return this.width / 2
-    },
-    halfHeight() {
-      return this.height / 2
-    },
-    halfPoint() {
-      return {
-        x: this.halfWidth,
-        y: this.halfHeight,
-      }
+    halfWidth: {
+      get() { return this.width / 2 },
+      set(val) { this.width = val * 2 }
     }
   }
 })
