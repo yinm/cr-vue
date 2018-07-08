4cd665bfedd2a4d364389f705efa0efe0534270a
yinm <yinm.cminor9@gmail.com>
2018-07-08 13:27:49 +0900

    Check watcher

--- src/main.js
+++ src/main.js
@@ -1,30 +1,18 @@
-new Vue({
+const app = new Vue({
   el: '#app',
   data: {
-    order: false,
-    budget: 300,
-    limit: 2,
+    edited: false,
     list: [
       { id: 1, name: 'apple', price: 100 },
       { id: 2, name: 'banana', price: 200 },
-      { id: 3, name: 'strawberry', price: 400 },
-      { id: 4, name: 'orange', price: 300 },
-      { id: 5, name: 'melon', price: 500 },
     ],
   },
-  computed: {
-    matched() {
-      return this.list.filter(el => {
-        return el.price <= this.budget
-      })
-    },
-
-    sorted() {
-      return _.orderBy(this.matched, 'price', this.order ? 'desc' : 'asc')
-    },
-
-    limited() {
-      return this.sorted.slice(0, this.limit)
-    },
+  created() {
+    const unwatch = this.$watch('list', () => {
+      this.edited = true
+      unwatch()
+    }, {
+      deep: true
+    })
   }
 })
