0453a65fb58f7823c2627bb34e7ef905b4eed09f
yinm <yinm.cminor9@gmail.com>
2018-07-08 09:41:45 +0900

    Check image file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 const app = new Vue({
   el: '#app',
   data: {
-    val: [],
+    preview: '',
   },
+  methods: {
+    handleChange(event) {
+      let file = event.target.files[0]
+      if (file && file.type.match(/^image\/(png|jpeg)$/)) {
+        this.preview = window.URL.createObjectURL(file)
+      }
+    }
+  }
 })
