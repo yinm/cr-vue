3672ecc7d3c79e434057a893a7277d41cff6c384
yinm <yinm.cminor9@gmail.com>
2018-07-08 13:50:29 +0900

    Check directives

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 new Vue({
   el: '#app',
-  filters: {
-    round(val) {
-      return Math.round(val * 100) / 100
-    },
-
-    radian(val) {
-      return val * Math.PI / 100
-    },
+  directives: {
+    focus: {
+      inserted(el) {
+        el.focus()
+      }
+    }
   }
 })
