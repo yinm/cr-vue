7bd52b0586f6aea1dc9ef61454019fa0da9bdf50
yinm <yinm.cminor9@gmail.com>
2018-07-08 12:52:54 +0900

    Check cache of computed property

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-const app = new Vue({
+new Vue({
   el: '#app',
-  data: {
-    width: 800,
-  },
   computed: {
-    halfWidth: {
-      get() { return this.width / 2 },
-      set(val) { this.width = val * 2 }
-    }
-  }
+    computedData() { return Math.random() },
+  },
+  methods: {
+    methodData() { return Math.random() },
+  },
 })
