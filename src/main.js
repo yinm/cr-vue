9ce0b744052fd0f6dbb998a97105fe1964d248a9
yinm <yinm.cminor9@gmail.com>
2018-07-10 08:22:31 +0900

    Check $refs

--- src/main.js
+++ src/main.js
@@ -1,34 +1,17 @@
 Vue.component('comp-child', {
-  template: '<li>{{ name }} HP.{{ hp }}\
-  <button @click="doAttack">攻撃する</button></li>',
-  props: {
-    id: Number,
-    name: String,
-    hp: Number,
-  },
-  methods: {
-    doAttack() {
-      this.$emit('attack', this.id)
-    }
+  template: '<div>test</div>',
+  created() {
+    this.$on('open', () => {
+      console.log('testttt')
+    })
   }
 })
 
 new Vue({
   el: '#app',
-  data: {
-    list: [
-      { id: 1, name: 'スライム', hp: 100 },
-      { id: 2, name: 'ゴブリン', hp: 200 },
-      { id: 3, name: 'ドラゴン', hp: 500 },
-    ],
-  },
   methods: {
-    handleAttack(id) {
-      let item = this.list.find((el) => {
-        return el.id === id
-      })
-
-      if (item !== undefined && item.hp > 0) item.hp -= 10
+    handleClick() {
+      this.$refs.child.$emit('open')
     }
   }
 })
