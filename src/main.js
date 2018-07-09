acac1195ff358671928b84a2490282aa151961eb
yinm <yinm.cminor9@gmail.com>
2018-07-10 08:52:57 +0900

    Check scoped slot

--- src/main.js
+++ src/main.js
@@ -1,17 +1,5 @@
 Vue.component('comp-child', {
-  template: `
-<section class="comp-child">
-  <header>
-    <slot name="header">デフォルトタイトル</slot> 
-  </header>  
-  <div class="content">
-    <slot>デフォルトコンテンツ</slot>
-  </div>
-  <slot name="footer">
-     <!-- なければ何も表示しない -->
-  </slot>
-</section>
-  `
+  template: '<div class=comp-child"><slot text="Hello!"></slot></div>',
 })
 
 new Vue({
