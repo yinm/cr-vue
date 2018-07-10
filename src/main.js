be6f127593bd0d4e939403888ba13baa21d462b2
yinm <yinm.cminor9@gmail.com>
2018-07-10 09:26:26 +0900

    Check dynamic component

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
-Vue.component('functional-component', {
-  functional: true,
-  render(createElement, context) {
-    return createElement('div', context.props.message)
-  },
-  props: {
-    message: String
-  },
+Vue.component('my-component-a', {
+  template: '<div class="my-component-a">component A</div>',
+})
+
+Vue.component('my-component-b', {
+  template: '<div class="my-component-b">component B</div>',
 })
 
 new Vue({
   el: '#app',
+  data: {
+    componentTypes: [
+      'my-component-a',
+      'my-component-b',
+    ],
+    current: 0,
+  },
+  computed: {
+    component() {
+      return this.componentTypes[this.current]
+    }
+  }
 })
