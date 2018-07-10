461c985dcdb3ab0bbdee5a5c1fbcea411cc616ed
yinm <yinm.cminor9@gmail.com>
2018-07-10 22:41:17 +0900

    Check transition

--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
-Vue.component('comp-board', {
-  template: '<div>Message Board</div>',
-})
-
-Vue.component('comp-form', {
-  template: '<div>Form<textarea v-model="message"></textarea></div>',
-  data() {
-    return { message: '' }
-  }
-})
-
 new Vue({
   el: '#app',
   data: {
-    current: 'comp-board',
+    show: true,
   },
 })
