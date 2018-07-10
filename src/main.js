f2f0ac52e64e9b956ec7b2fa4dba508f902e0de5
yinm <yinm.cminor9@gmail.com>
2018-07-10 09:04:42 +0900

    Check .sync

--- src/main.js
+++ src/main.js
@@ -1,7 +1,39 @@
-Vue.component('comp-child', {
-  template: '<div class=comp-child"><slot text="Hello!"></slot></div>',
+Vue.component('my-component', {
+  template: `
+    <div class="my-component">
+      <p>名前.{{ name }} HP.{{ hp }}</p>
+      <p>名前 <input v-model="localName"></p>
+      <p>HP <input size="5" v-model.number="localHp"></p>
+    </div>
+  `,
+  props: {
+    name: String,
+    hp: Number,
+  },
+  computed: {
+    localName: {
+      get() {
+        return this.name
+      },
+      set(val) {
+        this.$emit('update:name', val)
+      },
+    },
+    localHp: {
+      get() {
+        return this.hp
+      },
+      set(val) {
+        this.$emit('update:hp', val)
+      },
+    }
+  }
 })
 
 new Vue({
   el: '#app',
+  data: {
+    name: 'スライム',
+    hp: 100,
+  },
 })
