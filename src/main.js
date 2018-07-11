5daf20625d9e7949e38ba595c8b187d33b3b36ff
yinm <yinm.cminor9@gmail.com>
2018-07-11 23:58:39 +0900

    Vue plugin

--- src/main.js
+++ src/main.js
@@ -1,7 +1,33 @@
-const MyPlugin = {
-  install(Vue, options) {
-    console.log(options)
+const windowPlugin = {
+  install(Vue) {
+    let store = new Vue({
+      data: {
+        scrollY: 0,
+      },
+    })
+
+    let timer = null
+    window.addEventListener('scroll', function() {
+      if (timer === null) {
+        timer = setTimeout(function() {
+          store.scrollY = window.scrollY
+          clearTimeout(timer)
+          timer = null
+        }, 200)
+      }
+    })
+
+    Vue.prototype.$window = store.$data
   }
 }
 
-Vue.use(MyPlugin, { lang: 'ja' })
+Vue.use(windowPlugin)
+
+Vue.component('my-component', {
+  template: '<div>{{ scrollY }}</div>',
+  computed: {
+    scrollY() {
+      return this.$window.scrollY
+    },
+  },
+})
