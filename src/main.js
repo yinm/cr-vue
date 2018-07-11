1ba3ef9179573d5ff0ea2c046c375b8f59dbb236
yinm <yinm.cminor9@gmail.com>
2018-07-11 09:10:20 +0900

    Check list transition

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 new Vue({
   el: '#app',
   data: {
-    count: 0,
+    order: false,
+    list: [
+      { id: 1, name: 'apple', price: 100 },
+      { id: 2, name: 'banana', price: 200 },
+      { id: 3, name: 'strawberry', price: 300 },
+    ],
+  },
+  computed: {
+    sortedList() {
+      return _.orderBy(this.list, 'price', this.order ? 'desc' : 'asc')
+    },
   },
 })
