a6ed28a39812934d3ed7632e6798c40c4932c565
yinm <yinm.cminor9@gmail.com>
2018-07-12 23:16:26 +0900

    Check named module

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -2,13 +2,5 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
-console.log(store.state.moduleA.count)
-console.log(store.state.moduleB.count)
-
-store.commit('moduleA/update')
-console.log(store.state.moduleA.count)
-console.log(store.state.moduleB.count)
-
-store.commit('moduleB/update')
-console.log(store.state.moduleA.count)
-console.log(store.state.moduleB.count)
+store.dispatch('moduleA/test')
+console.log(store.getters['moduleA/test'])
--- my-app/src/store.js
+++ my-app/src/store.js
@@ -4,32 +4,51 @@ Vue.use(Vuex)
 
 const moduleA = {
   namespaced: true,
-  state: {
-    count: 1
+  getters: {
+    test(state, getters, rootState, rootGetters) {
+      // 自分自身の item ゲッターを使用 getters['moduleA/item']
+      getters.item
+      // ルートの user ゲッターを使用
+      rootGetters.user
+
+      return [getters.item, rootGetters.user]
+    },
+    item() { return 'getter: moduleA/item' },
   },
-  mutations: {
-    update(state) {
-      state.count += 100
-    }
+  actions: {
+    test({ dispatch, commit, getters, rootGetters }) {
+      // 自分自身の update をディスパッチ
+      dispatch('update')
+      // ルートの update をディスパッチ
+      dispatch('update', null, { root: true })
+      // ルートの update をコミット
+      commit('update', null, { root: true })
+      // ルートに登録されたモジュール moduleB の update をコミット
+      commit('moduleB/update', null, { root: true })
+    },
+    update() { console.log('action: moduleA/update') },
   }
 }
-
 const moduleB = {
   namespaced: true,
-  state: {
-    count: 2
-  },
   mutations: {
-    update(state) {
-      state.count += 200
-    }
+    update() { console.log('mutation: moduleB/update') }
   }
 }
 
 const store = new Vuex.Store({
   modules: {
     moduleA,
-    moduleB,
+    moduleB
+  },
+  getters: {
+    user() { return 'getter: user' }
+  },
+  mutations: {
+    update() { console.log('mutation: update') }
+  },
+  actions: {
+    update() { console.log('action: update') }
   }
 })
 
