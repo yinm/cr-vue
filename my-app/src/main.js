6b252dc8c6580fbd2947d7326bb51ccaee395d9e
yinm <yinm.cminor9@gmail.com>
2018-07-12 19:25:43 +0900

    Check store

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,13 +1,5 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import store from '@/store.js'
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+console.log(store.state.count)
+store.commit('increment')
+console.log(store.state.count)
--- my-app/src/store.js
+++ my-app/src/store.js
@@ -3,3 +3,16 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    }
+  }
+})
+
+export default store
