1806d4c6a45dba52cc826571571864e08915d3b6
yinm <yinm.cminor9@gmail.com>
2018-07-12 23:27:10 +0900

    Check reuse modules

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -2,5 +2,5 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
-store.dispatch('moduleA/test')
-console.log(store.getters['moduleA/test'])
+store.dispatch('moduleA/load', '/path/a.json')
+store.dispatch('moduleB/load', '/path/b.json')
--- my-app/src/store.js
+++ my-app/src/store.js
@@ -1,54 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 Vue.use(Vuex)
 
-const moduleA = {
+const myModule = {
   namespaced: true,
-  getters: {
-    test(state, getters, rootState, rootGetters) {
-      // 自分自身の item ゲッターを使用 getters['moduleA/item']
-      getters.item
-      // ルートの user ゲッターを使用
-      rootGetters.user
-
-      return [getters.item, rootGetters.user]
-    },
-    item() { return 'getter: moduleA/item' },
+  state() {
+    return {
+      entries: []
+    }
+  },
+  mutations: {
+    set(state, payload) {
+      state.entries = payload
+    }
   },
   actions: {
-    test({ dispatch, commit, getters, rootGetters }) {
-      // 自分自身の update をディスパッチ
-      dispatch('update')
-      // ルートの update をディスパッチ
-      dispatch('update', null, { root: true })
-      // ルートの update をコミット
-      commit('update', null, { root: true })
-      // ルートに登録されたモジュール moduleB の update をコミット
-      commit('moduleB/update', null, { root: true })
-    },
-    update() { console.log('action: moduleA/update') },
-  }
-}
-const moduleB = {
-  namespaced: true,
-  mutations: {
-    update() { console.log('mutation: moduleB/update') }
+    load({ commit }, file) {
+      axios.get(file).then(response => {
+        commit('set', response.data)
+      })
+    }
   }
 }
 
 const store = new Vuex.Store({
   modules: {
-    moduleA,
-    moduleB
-  },
-  getters: {
-    user() { return 'getter: user' }
-  },
-  mutations: {
-    update() { console.log('mutation: update') }
-  },
-  actions: {
-    update() { console.log('action: update') }
+    moduleA: myModule,
+    moduleB: myModule,
   }
 })
 
